ab7193cfd3f33d3de2afd4e9a3a7cd2d89a78263
Moein Madadi <moeinmadadi2002@gmail.com>
2024-11-23 21:50:36 +0330

    Complete Calculator class

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,6 +2,20 @@ package calculator;
 
 public class Calculator {
     public static double calculate(double operand1, double operand2, String operator) {
-        return 1.0;
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) {
+                    throw new IllegalArgumentException("Division by zero");
+                }
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
     }
 }
